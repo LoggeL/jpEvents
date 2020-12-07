946bd5eed044a6ddff4f579eb216dbcc2551c1e9
LoggeL <hyper.xjo@gmx.de>
2020-12-08 00:44:32 +0100

    first usable version

--- js/main.js
+++ js/main.js
@@ -1,48 +1,65 @@
-function calendar() {
-    document.getElementById('table').innerHTML = ' <thead><tr id="theadtr"></tr></thead>'
-    const tbody = document.createElement('tbody')
-    const thead = document.getElementById('theadtr')
-    const calendarURL = document.getElementById('url').value
-    fetch('https://calendar.logge.workers.dev/' + calendarURL).then(response => response.text().then(icsString => {
+dayjs.extend(dayjs_plugin_relativeTime)
 
-        console.log(icsString)
+const calendarURL = 'https://cloud.jupeters.de/remote.php/dav/public-calendars/rgNpg4W5S92DytyZ?export'
 
-        let events = []
-        let collector = {}
-        let start = false
-        const lines = icsString.split("\n");
-        for (i = 0; i < lines.length; i++) {
-            const line = lines[i]
-            if (line.startsWith('BEGIN:VEVENT')) {
-                if (Object.keys(collector).length > 0) events.push(collector)
-                collector = {}
-                start = true
-            }
-            else if (!start) continue
-            else {
-                let split = line.split(':')
-                const property = split.shift().replace(';VALUE=DATE', '')
-                collector[property] = split.join(':').trim().replace('\\,', ',')
-            }
-        }
+const tbody = document.createElement('tbody')
+const thead = document.getElementById('theadtr')
 
-        const keys = Object.keys(events[0])
-        for (let i = 0; i < keys.length; i++) {
-            const th = document.createElement('th')
-            th.innerText = keys[i]
-            thead.append(th)
-        }
-
-        for (let i = 0; i < events.length; i++) {
-            const tr = document.createElement('tr')
-            const values = Object.values(events[i])
-            for (let l = 0; l < values.length; l++) {
-                const td = document.createElement('td')
-                td.innerText = values[l]
-                tr.append(td)
-            }
-            tbody.append(tr)
-        }
-        document.getElementById('table').append(tbody)
-    }))
+const validKeys = ['RELATIVE', 'DTSTART', 'DTEND', 'SUMMARY', 'DESCRIPTION']
+const translateKeys = {
+    'RELATIVE': 'Wann',
+    'DTSTART': 'Begin',
+    'DTEND': 'Ende',
+    'SUMMARY': 'Event',
+    'DESCRIPTION': 'Infos'
 }
+
+fetch('https://calendar.logge.workers.dev/' + calendarURL).then(response => response.text().then(icsString => {
+
+    let events = []
+    let collector = {}
+    let start = false
+    const lines = icsString.split("\n");
+    for (i = 0; i <= lines.length; i++) {
+        const line = lines[i]
+        if (!line || line.startsWith('BEGIN:VEVENT')) {
+            if (Object.keys(collector).length > 0) events.push(collector)
+            collector = {}
+            start = true
+        }
+        else if (!start) continue
+        else {
+            let split = line.split(':')
+            const property = split.shift().split(';')[0]
+            let value = split.join(':').trim().replace('\\,', ',')
+            if (property === "DTSTART") {
+                console.log(value, dayjs(value).fromNow())
+                collector['RELATIVE'] = dayjs(value).fromNow()
+            }
+            if (property === "DTSTART" || property === "DTEND") {
+                const date = dayjs(value)
+                if (date.hour() == "0" && date.minute() == "0") value = date.format('DD/MM/YYYY')
+                else value = date.format('DD/MM/YYYY HH:mm')
+            }
+            collector[property] = value
+        }
+    }
+
+    for (let i = 0; i < validKeys.length; i++) {
+        const th = document.createElement('th')
+        th.innerText = translateKeys[validKeys[i]]
+        thead.append(th)
+    }
+    console.log(events)
+
+    for (let i = 0; i < events.length; i++) {
+        const tr = document.createElement('tr')
+        for (let l = 0; l < validKeys.length; l++) {
+            const td = document.createElement('td')
+            td.innerText = events[i][validKeys[l]] || ''
+            tr.append(td)
+        }
+        tbody.append(tr)
+    }
+    document.getElementById('table').append(tbody)
+}))
